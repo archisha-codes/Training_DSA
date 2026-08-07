b70e36d965ab9edc990071fa1cd8c75842be1ea4
Archisha Ranjan <forcollegearchi@gmail.com>
2026-08-07 21:22:12 +0530

    Time: 25 ms (19.72%), Space: 65.3 MB (77.86%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int prefixSum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 0)
+                prefixSum--;
+            else
+                prefixSum++;
+
+            if (map.containsKey(prefixSum)) {
+                maxLength = Math.max(maxLength, i - map.get(prefixSum));
+            } else {
+                map.put(prefixSum, i);
+            }
+        }
+
+        return maxLength;
+    }
+}
