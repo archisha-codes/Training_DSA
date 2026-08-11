fb3d9fa9da514f9b26a4fe97dde1b512392774d9
Archisha Ranjan <forcollegearchi@gmail.com>
2026-08-11 10:15:04 +0530

    Time: 0 ms (100%), Space: 48.4 MB (38.18%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -3,7 +3,7 @@ class Solution {
         int left = 0;
         int right = s.length - 1;
 
-        while (left < right) {
+        while (left <= right) {
             char temp = s[left];
             s[left] = s[right];
             s[right] = temp;
